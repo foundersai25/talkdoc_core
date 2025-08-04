22706bd004043dde8a8d4f4464d42e4a798cd5a6
dkr <deepukreddy123@gmail.com>
2025-08-04 19:45:13 +0200

    new burgergeld

--- scripts/extract_form.py
+++ scripts/extract_form.py
@@ -1,5 +1,5 @@
 from talkdoc_core.pdf_ops import extract_fields_from_form
 
-pdf_path = "pdfs/Buergergeld_Antrag.pdf"
+pdf_path = "pdfs/Buergergeld_Antrag_v3.pdf"
 
 extract_fields_from_form(pdf_path)
